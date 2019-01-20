57960feacaef00f4668d6d740d13990a374b0f67
Timothy Millward <silhouettetechnician@Timothys-MacBook-Pro.local>
2019-01-20 00:07:09 +0000

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
