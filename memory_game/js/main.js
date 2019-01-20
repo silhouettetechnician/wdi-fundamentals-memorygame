491048feea9f03c7d568dd56e016338ec6ab79ba
Timothy Millward <silhouettetechnician@Timothys-MacBook-Pro.local>
2019-01-20 22:55:23 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,51 @@
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne  = cards[0];
 
-cardsInPlay.push(cardOne);
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match!");
+	} else{
+		console.log("Sorry try again.");
+	}
+}
 
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
+var flipCard = function(cardId){
+console.log("User flipped " + cards[cardId]);
 
 if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match");
-	}
-		else{
-			alert("Sorry try again");
-		}
-		
-	}
+	checkForMatch()
+}
+	// if (cardsInPlay[0] === cardsInPlay[1]){
+	// 	alert("You found a match!");
+	// } else{
+	// 	alert("Sorry try again.");
+	// }
+
+
+cardsInPlay.push(cards[cardId]);
+}
+// 
+
+// var flipCard = function(cardId){
+
+// if (cardsInPlay.length === 2){
+// 	checkForMatch();
+// 	}
+
+// 	console.log("user flipped " + cards[cardId]);
+
+// 	cardsInPlay.push(cards[cardId]);
+// }
+
+
+		flipCard(0);
+		flipCard(2);
 
 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+
+
+
+
