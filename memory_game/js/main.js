697e62aeb525bc82c4a16172e937c5f161634e82
Timothy Millward <silhouettetechnician@timothys-mbp.lan>
2019-01-20 13:49:54 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,25 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
 
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne  = cards[0];
+
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match");
+	}
+		else{
+			alert("Sorry try again");
+		}
+		
+	}
+
+
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
